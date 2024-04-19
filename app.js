f740b1c2b983e7a115db5bc75336f80b2575381e
MishaBozhevskiy <misha.bozhev@gmail.com>
2024-04-19 22:58:18 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`  + ` // Сервер запущен на ${PORT} порту`);
+})
